59ea216ac6ac0de6fd5cea6f1b0a06084138f349
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-10-18 16:37:03 -0400

    Create SearchThis.java

---
+++ SearchThis.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import javax.swing.*;
+
+// class 
+public class SearchThis{
+    // class constructor
+    SearchThis() {
+        // declare a frame to house the buttons
+        JFrame windowFrame = new JFrame();
+
+        // add button1
+        JButton button1 = new JButton("Date");
+        windowFrame.add(button1);   
+
+        // add button2
+        JButton button2 = new JButton("Amount");
+        windowFrame.add(button2);
+
+        // set layout, size, and visbility
+        windowFrame.setLayout(new FlowLayout());
+        windowFrame.setSize(300, 300);    
+        windowFrame.setVisible(true);
+    }
+
+    // main
+    public static void main(String args[]){
+        // calls for a new instance of the SearchThis class
+        new SearchThis();
+    }
+}
