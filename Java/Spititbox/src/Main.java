a261ba49ea83a911cf1ddf1f94a714724c66ff19
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-12-05 21:47:59 -0500

    Added more content

---
+++ Java/Spititbox/src/Main.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        Indicator indicator = new Indicator();
+        indicator.getFirstSeen();
+        indicator.setFirstSeen();
+    }
+}
