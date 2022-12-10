0982c0f16b2e4831e84bc56e12960aea24541420
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2022-12-09 23:53:52 -0500

    Delete Java/Spititbox directory

--- Java/Spititbox/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime;
-
-public class Main {
-    public static void main(String[] args) {
-        Indicator indicator = new Indicator();
-        indicator.getFirstSeen();
-        indicator.setFirstSeen();
-    }
-}
